7906f24274da95dc3df83e31876822e219dd9281
David Ochsner <30530196+ochsnerd@users.noreply.github.com>
2026-03-28 13:25:40 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs.nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          DOTNET_WATCH_RESTART_ON_RUDE_EDIT = "1";
+          DOTNET_WATCH_SUPPRESS_LAUNCH_BROWSER = "1";
+          DOTNET_ENVIRONMENT = "Development";
+          buildInputs = with pkgs; [
+            dotnet-sdk_10
+            csharpier
+          ];
+        };
+      }
+    );
+}
